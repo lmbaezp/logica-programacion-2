9a6943ad2c8ef613aa3459f0b3e6fc24eb394964
LMBP <juenpame@gmail.com>
2025-08-12 17:23:18 -0500

    primer comit

---
+++ main.js
@@ -0,0 +1,10 @@
+const tempCelsius = document.getElementById('tempCelsius');
+const btnsConvertir = document.querySelectorAll('.btn-convertir');
+console.log(btnsConvertir)
+
+btnsConvertir.forEach(btn => {
+    btn.addEventListener
+    
+});
+
+
