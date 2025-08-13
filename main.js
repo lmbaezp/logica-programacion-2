2145bbceab663e1c802e2be112eb0b85afbd2b8b
LMBP <juenpame@gmail.com>
2025-08-12 20:59:50 -0500

    tarea logica de programacion 2

--- main.js
+++ main.js
@@ -1,10 +1,35 @@
 const tempCelsius = document.getElementById('tempCelsius');
 const btnsConvertir = document.querySelectorAll('.btn-convertir');
-console.log(btnsConvertir)
+const containerResultado = document.getElementById('resultado');
 
 btnsConvertir.forEach(btn => {
-    btn.addEventListener
-    
+    btn.addEventListener('click', () => {
+        containerResultado.value = '';
+        console.log(isNaN(tempCelsius.value))
+
+        if (!/^-?\d+(\.\d{1,2})?$/.test(tempCelsius.value.trim())) {
+            tempCelsius.classList.add('is-invalid')
+        }
+        else {
+            const inputValue = Number(tempCelsius.value);
+            const inputTemperatura = Number.isInteger(inputValue) ? parseInt(inputValue) : parseFloat(inputValue);
+            let resultado;
+            let escala;
+            switch (btn.getAttribute('escala')) {
+                case 'farenheit':
+                    resultado = (inputTemperatura * 1.8) + 32;
+                    escala = 'F';
+                    break;
+                case 'kelvin':
+                    resultado = inputTemperatura + 273, 15;
+                    escala = 'K';
+                    break;
+            }
+            containerResultado.value = `${tempCelsius.value}°C = ${resultado}°${escala}`;
+            tempCelsius.classList.remove('is-invalid')
+        }
+    });
+
 });
 
 
